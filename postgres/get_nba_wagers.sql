1b9b737f13140fb6616176f2817869e22fa09c48
tomney <gregory.tomney@gmail.com>
2022-08-15 20:46:18 -0400

    query to get nba wagers in june 2019

---
+++ postgres/get_nba_wagers.sql
@@ -0,0 +1,35 @@
+--  1. Find the account_ids, number of bets placed 
+--  and total dollars wagered on NBA games in
+--  June 2019. Order the results in descending order by wager amount.
+WITH june_2019_nba_games AS (
+    SELECT id
+    FROM games
+    WHERE league = 'NBA' 
+        AND game_time >= MAKE_TIMESTAMP(2019,6,1,0,0,0) 
+        AND game_time < MAKE_TIMESTAMP(2019,7,1,0,0,0)
+)
+, june_2019_nba_bet_legs AS (
+    SELECT
+        bet_legs.id 
+        ,bet_id 
+        ,game_id
+    FROM bet_legs
+    INNER JOIN june_2019_nba_games jg ON jg.id = bet_legs.game_id
+)
+, june_2019_nba_bets AS (
+    SELECT 
+        bets.id
+        ,account_id
+        ,amount 
+    FROM bets
+    INNER JOIN june_2019_nba_bet_legs jl ON jl.bet_id = bets.id
+)
+
+SELECT
+    account_id
+    ,COUNT(account_id) as num_bets
+    ,COUNT(amount) as total_waged
+FROM june_2019_nba_bets
+GROUP BY account_id
+ORDER BY
+    total_waged DESC
