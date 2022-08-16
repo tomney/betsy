f13e3906831b1569f16eba83c91a54131588577a
tomney <gregory.tomney@gmail.com>
2022-08-16 09:10:52 -0400

    cumulative total for 2019 bets january to june

---
+++ get_cumulative_total.sql
@@ -0,0 +1,27 @@
+--  4. Write a query that returns the total dollars bet monthly from 
+--  Jan 2019 to June 2019 (inclusive) as well as the cumulative total 
+--  dollars bet over the 6 month period. The resulting columns
+--  should be: month, monthly_total, cumulative_total.
+
+WITH jan_jun_2019_monthly_bets AS (
+    SELECT
+        EXTRACT(month FROM bet_timestamp) AS bet_month
+        ,amount
+    FROM bets
+    WHERE bet_timestamp >= MAKE_TIMESTAMP(2019,1,1,0,0,0) 
+        AND bet_timestamp < MAKE_TIMESTAMP(2019,7,1,0,0,0)
+)
+, jan_jun_2019_monthly_bet_totals AS (
+    SELECT
+        bet_month
+        ,SUM(amount) AS monthly_total
+    FROM jan_jun_2019_monthly_bets
+    GROUP BY 
+        bet_month
+)
+SELECT
+    bet_month,
+    monthly_total,
+    SUM(monthly_total) OVER (PARTITION BY bet_month ORDER BY bet_month) AS cumulative_total
+FROM jan_jun_2019_monthly_bet_totals
+
--- main.py
+++ main.py
@@ -4,6 +4,3 @@ import psycopg2
 
 
 
-# 4. Write a query that returns the total dollars bet monthly from Jan 2019 to June 2019 (inclusive)
-# as well as the cumulative total dollars bet over the 6 month period. The resulting columns
-# should be: month, monthly_total, cumulative_total.
